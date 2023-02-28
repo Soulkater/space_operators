c841fb66291c5ba965e99ea84194a2832631e2cc
Outougane Mohamed <outougane.moh@gmail.com>
2023-02-28 09:27:34 +0100

    feat(view) : ajout css pour la page création partie

---
+++ src/pages/CreateSession/style.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components/native';
+
+export const CreateSessionContent = styled.View`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+`;
+
+export const CreateSessionPlayersTitleWrapper = styled.Text`
+    background-color: #dc2fbe82;
+    padding: 10px;
+    border-radius: 10px;
+`;
+
+export const CreateSessionTitle = styled.Text`
+    width: 70%;
+    color: #fff;
+`;
+
+export const CreateSessionWrapper = styled.Text`
+
+`;
+
+export const CreateSessionPlayersTitle = styled.Text`
+
+`;
+
+export const CreateSessionPlayersWrapper = styled.Text`
+
+`;
+
+export const CreateSessionPlayerName = styled.Text`
+
+`;
+
+export const CreateSessionButtonWrapper = styled.Text`
+
+`;
+
+export const CreateSessionButtonStart = styled.Text`
+
+`;
+
+export const CreateSessionButtonQuit = styled.Text`
+
+`;
